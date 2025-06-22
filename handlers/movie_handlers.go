f4503b6c0a439a7600c0c7724c305d93381d3ac2
JeremyDwayne <jeremy@jeremydwayne.com>
2025-06-22 12:41:53 -0400

    Enhance movie API with PostgreSQL integration and repository pattern
    - Added PostgreSQL database connection in main.go with environment variable support.
    - Created MovieRepository for data access, implementing methods to fetch top and random movies.
    - Introduced MovieStorage interface for better abstraction in movie data handling.
    - Updated MovieHandler to utilize the new repository for fetching movie data.
    - Added database schema dump for initial setup.
    - Implemented install.go for executing SQL scripts to set up the database.

--- handlers/movie_handlers.go
+++ handlers/movie_handlers.go
@@ -4,42 +4,38 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/jeremydwayne/FEM_movies/models"
+	"github.com/jeremydwayne/FEM_movies/data"
+	"github.com/jeremydwayne/FEM_movies/logger"
 )
 
-type MovieHandler struct{}
+type MovieHandler struct {
+	Storage data.MovieStorage
+	Logger  *logger.Logger
+}
 
-func (m *MovieHandler) writeJSON(w http.ResponseWriter, data interface{}) {
+func (m *MovieHandler) writeJSON(w http.ResponseWriter, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(data); err != nil {
+		m.Logger.Error("JSON Encoding error", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func (m *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
-	movies := []models.Movie{
-		{
-			ID:          1,
-			Title:       "The Matrix",
-			ReleaseYear: 1999,
-			Genres: []models.Genre{
-				{ID: 1, Name: "Action"},
-				{ID: 2, Name: "Sci-Fi"},
-			},
-			Keywords: []string{},
-			Casting:  []models.Actor{{ID: 1, FirstName: "Keanu", LastName: "Reeves"}},
-		},
-		{
-			ID:          2,
-			Title:       "Back to the Future",
-			ReleaseYear: 1984,
-			Genres: []models.Genre{
-				{ID: 1, Name: "Action"},
-				{ID: 2, Name: "Sci-Fi"},
-			},
-			Keywords: []string{},
-			Casting:  []models.Actor{{ID: 2, FirstName: "John", LastName: "Doe"}},
-		},
+	movies, err := m.Storage.GetTopMovies()
+	if err != nil {
+		http.Error(w, "Internal Error", http.StatusInternalServerError)
+		m.Logger.Error("Unable to retrieve Top Movies", err)
+	}
+
+	m.writeJSON(w, movies)
+}
+
+func (m *MovieHandler) GetRandomMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := m.Storage.GetRandomMovies()
+	if err != nil {
+		http.Error(w, "Internal Error", http.StatusInternalServerError)
+		m.Logger.Error("Unable to retrieve Random Movies", err)
 	}
 
 	m.writeJSON(w, movies)
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/jeremydwayne/FEM_movies/data"
 	"github.com/jeremydwayne/FEM_movies/handlers"
 	"github.com/jeremydwayne/FEM_movies/logger"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func initializeLogger() *logger.Logger {
@@ -18,14 +23,44 @@ func initializeLogger() *logger.Logger {
 }
 
 func main() {
+	// Logging
 	logInstance := initializeLogger()
-	movieHandler := handlers.MovieHandler{}
+
+	// Environment Variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	connStr := os.Getenv("DATABASE_URL")
+
+	if connStr == "" {
+		log.Fatal("DATABASE_URL NOT SET")
+	}
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to the DB: %v", err)
+	}
+
+	defer db.Close()
+
+	// Initialize Repositories
+	movieRepo, err := data.NewMovieRepository(db, logInstance)
+	if err != nil {
+		log.Fatalf("Failed to initialize repository")
+	}
+
+	// Handlers
+	movieHandler := handlers.MovieHandler{
+		Storage: movieRepo,
+		Logger:  logInstance,
+	}
 
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.HandleFunc("/api/movies/top", movieHandler.GetTopMovies)
+	http.HandleFunc("/api/movies/random", movieHandler.GetRandomMovies)
+
 	const addr = ":8080"
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		logInstance.Error("Server failed: %v", err)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		logInstance.Error("Server failed", err)
 	}
 }
